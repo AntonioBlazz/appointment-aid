c3fff00b66eae0eb6f81c6316e95c5db3e43b8cc
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-10-29 20:38:25 +0100

    backend

--- backend/routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { addDoctor } from '../controllers/adminController,js'
-import upload from '../middlewares/multer'
-
-const adminRouter = express.Router()
-
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
-
-export default adminRouter
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongoDB.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
+
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
 
-// middlewares
+//middlewares
 app.use(express.json())
 app.use(cors())
 
-// api endpoint
-app.use('/api/admin', adminRouter)
-
+// api endpoints
 
 app.get('/',(req,res)=>{
-  res.send('API WORKING')
+    res.send('API WORKING')
 })
 
-app.listen(port, ()=> console.log("Server Started", port))
+app.listen(port, ()=> console.log("Server Started",port))
