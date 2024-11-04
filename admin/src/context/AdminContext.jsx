1b8c524baf2dd9dfd5b94008105d7fa39830bb6a
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-11-04 20:32:24 +0100

    admin login created

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react"
+
+export const AdminContext = createContext()
+
+const AdminContextProvider = (props) => {
+
+    const [aToken,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
+
+    const backendUrl = 'http://localhost:4000'
+    const value = {
+        aToken,setAToken,
+        backendUrl,
+    }
+
+    return (
+        <AdminContext.Provider value={value}>
+            {props.children}
+        </AdminContext.Provider>
+    )
+}
+
+
+export default AdminContextProvider
